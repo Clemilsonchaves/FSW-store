eaeeef302297106d01bc74d3411a5b34ae243617
clemilson chaves <clemilsonchaves.1992@gmail.com>
2023-10-22 17:14:40 -0300

    feat:  add button, card and header components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
 import Image from 'next/image'
 
 export default function Home() {
- return ( 
-  <div  className="text-red-500  bg-green-500" >
-
-  Hello World!
-
-  </div>
-
-  );
+ return (
+  <div>home</div>
   
-  
+ );
 }
